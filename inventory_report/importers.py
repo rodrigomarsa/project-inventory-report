54a01882c584ed20bb04d16b367fe4ccb2b17032
Rodrigo <rodrigomartins.agro@gmail.com>
2023-07-14 14:41:09 -0300

    feat: add class CsvImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,4 @@
+import csv
 import json
 from typing import Dict, Type
 from abc import ABC, abstractmethod
@@ -36,8 +37,27 @@ class JsonImporter(Importer):
             raise FileNotFoundError(f"Arquivo inexistente: {self.path}")
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> list[Product]:
+        products = []
+        try:
+            with open(self.path, encoding="utf-8") as file:
+                data_products = csv.reader(file, delimiter=",")
+                header, *data = data_products
+            for row in data:
+                new_product = Product(
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5],
+                    row[6],
+                )
+                products.append(new_product)
+            return products
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Arquivo inexistente: {self.path}")
 
 
 # Não altere a variável abaixo
